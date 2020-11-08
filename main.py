194675a555d9c9359621d4ec2960e13288d1ec6b
Peiran Quan <quanpeiran@gmail.com>
2020-11-09 11:48:39 +1300

    before moving my dir

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
 import sys
 
+print("start!")
 if os.path.exists('src.zip'):
+    print(sys.path)
     sys.path.insert(0, 'src.zip')
+    print(sys.path)
 else:
     sys.path.insert(0, './src')
 
