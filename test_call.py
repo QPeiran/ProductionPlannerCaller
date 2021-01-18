c3db9e2bcebaaa9f8b2bf8d77b8f34a373f2940d
Peiran Quan <quanpeiran@gmail.com>
2021-01-19 12:13:35 +1300

    test_calling

--- test_call.py
+++ test_call.py
@@ -1,30 +1,27 @@
 import requests
 import json
 ACCESS_TOKEN = "m1wq_i9NiwEW4jym3s_MX6mpJgFLND2bERLtsf-8"
+URL = "https://api.predicthq.com/v1/events/"
 
-
-
-def get_nest_data(token):
-    url = "https://api.predicthq.com/v1/events/"
-    auth_t = token#.encode("ascii", "ignore")
+def get_data(token,url):
     headers = {
-        'authorization': "Bearer " + auth_t,
+        'authorization': "Bearer " + token,
         'content-type': "application/json",
     }
 
     try:
-        init_res = requests.get('https://api.predicthq.com/v1/events/', headers=headers)
+        init_res = requests.get(URL, headers=headers)
         if init_res.status_code == 200:
             return init_res.json()
     except Exception as ce:
         print(ce)
 
-print(get_nest_data(ACCESS_TOKEN))
+print(get_data(ACCESS_TOKEN, URL))
 
 
 # response = requests.get(
 #     url="https://api.predicthq.com/v1/events/",
-#     headers={"authorization": "Bearer $ACCESS_TOKEN"},
+#     headers={"Authorization": "Bearer $ACCESS_TOKEN"},
 #     params={"q": "jazz"},
 #     allow_redirects=False
 # )
