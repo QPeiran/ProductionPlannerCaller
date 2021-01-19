9d33384c75cb64bb2dbcbbd185f6ecf426a1d661
Peiran Quan <quanpeiran@gmail.com>
2021-01-19 16:02:35 +1300

    compare calls

--- test_call.py
+++ test_call.py
@@ -2,28 +2,29 @@ import requests
 import json
 ACCESS_TOKEN = "m1wq_i9NiwEW4jym3s_MX6mpJgFLND2bERLtsf-8"
 URL = "https://api.predicthq.com/v1/events/"
+params={
+        "q": "jazz",
+        "country": "NZ"
+        }
 
-def get_data(token,url):
+def get_data(token,url,params):
     headers = {
         'authorization': "Bearer " + token,
         'content-type': "application/json",
-    }
-
+        }
     try:
-        init_res = requests.get(URL, headers=headers)
+        init_res = requests.get(URL, headers=headers, params=params)
         if init_res.status_code == 200:
             return init_res.json()
     except Exception as ce:
         print(ce)
 
-print(get_data(ACCESS_TOKEN, URL))
+print(get_data(ACCESS_TOKEN, URL, params))
 
 
 # response = requests.get(
 #     url="https://api.predicthq.com/v1/events/",
-#     headers={"Authorization": "Bearer $ACCESS_TOKEN"},
-#     params={"q": "jazz"},
-#     allow_redirects=False
+#     headers={"Authorization": "Bearer $ACCESS_TOKEN"}
 # )
 
 # print(response.json())
